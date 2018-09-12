c9b561371535d466e91d9ac7ec1a8b6661f02616
Chandan Singh <chandan_singh@berkeley.edu>
2018-09-11 22:35:36 -0700

    basic tracking plots

--- viz/viz.py
+++ viz/viz.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 
-def plot_data(X, y_scalar):
+def plot_data(X, y_scalar, show=True):
     plt.scatter(X, y_scalar)
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.show()
+    if show:
+        plt.show()
