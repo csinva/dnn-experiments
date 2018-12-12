60fc965c038569eb5010d5c13cd39b14b2dc97e3
Chandan Singh <chandan_singh@berkeley.edu>
2018-12-11 21:12:03 -0800

    plot comparing max corrs of different nets

--- scripts/submit_max_corrs.py
+++ scripts/submit_max_corrs.py
@@ -8,7 +8,7 @@ s = Slurm("cnn_corr", {"partition": "gpu_yugroup", "time": "4-0", "gres": "gpu:1
 models = ['inception_v3']
 # [['alexnet', 'inception_v3'],
 # ['vgg11', 'vgg13', 'vgg16', 'vgg19'],
-# ['resnet18', 'resnet34', 'resnet50'],                    
+# ['resnet18', 'resnet34', 'resnet50'], ['resnet101', 'resnet152']                    
 # ['densenet121', 'densenet169', 'densenet201']]
 
 # iterate
