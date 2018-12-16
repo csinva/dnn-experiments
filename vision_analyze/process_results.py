93065c19eb64ebe1dff59d27eac10ec71f7392c6
Chandan Singh <chandan_singh@berkeley.edu>
2018-12-16 11:48:34 -0800

    add 4lay fig

--- vision_analyze/process_results.py
+++ vision_analyze/process_results.py
@@ -35,6 +35,8 @@ def process_results(results):
     t_max_w = int(max(ts))
     corr0, corr0_adj, corr1, corr1_adj = [], [], [], []
     fc0_fro, fc1_fro, fc0_stab_rank, fc1_stab_rank = [], [], [], []
+    corr2, corr3 = [], []
+    fc2_fro, fc3_fro = [], []
     act0_stab_rank, act1_stab_rank = [], []
     for _, row in results.iterrows():
         mem_stat_dict0 = [row['mean_max_corrs'][t]['fc.0.weight'] for t in ts]
@@ -50,11 +52,28 @@ def process_results(results):
         fc1_stab_rank.append(np.apply_along_axis(stable_rank, axis=1, arr=[try_key_pca(row, 'singular_val_dicts', t)['fc.1.weight'] for t in ts]))
         act0_stab_rank.append(np.apply_along_axis(stable_rank, axis=1, arr=[try_key_pca(row, 'act_singular_val_dicts_train', t)['fc.0'] for t in ts]))
         act1_stab_rank.append(np.apply_along_axis(stable_rank, axis=1, arr=[try_key_fc2(try_key_pca(row, 'act_singular_val_dicts_train', t)) for t in ts]))
+        
+        
+        
+        if row.num_layers >= 4:
+            mem_stat_dict2 = [row['mean_max_corrs'][t]['fc.2.weight'] for t in ts]
+            corr2.append([np.mean(d['max_corrs']) for d in mem_stat_dict2])
+            mem_stat_dict3 = [row['mean_max_corrs'][t]['fc.3.weight'] for t in ts]
+            corr3.append([np.mean(d['max_corrs']) for d in mem_stat_dict3])
 
+            fc2_fro.append([row['weight_norms'][t]['fc.2.weight_fro'] for t in ts])
+            fc3_fro.append([row['weight_norms'][t]['fc.3.weight_fro'] for t in ts])
+        else:
+            corr2.append(None)
+            corr3.append(None)
+            fc2_fro.append(None)
+            fc3_fro.append(None)                
 
     # array summaries    
     results['fc0_fro'] = fc0_fro
     results['fc1_fro'] = fc1_fro
+    results['fc2_fro'] = fc2_fro
+    results['fc3_fro'] = fc3_fro
     results['fc0_stab_rank'] = fc0_stab_rank
     results['fc1_stab_rank'] = fc1_stab_rank
     results['act0_stab_rank'] = act0_stab_rank
@@ -63,23 +82,22 @@ def process_results(results):
     results['corr1'] = corr1
     results['corr0_adj'] = corr0_adj
     results['corr1_adj'] = corr1_adj
+    results['corr2'] = corr2
+    results['corr3'] = corr3
 
     # scalar summaries
     idxs = results.index
-    results['max_train_acc'] = np.array([max(results.accs_train[i]) for i in idxs ]) 
+    results['max_train_acc'] = np.array([max(results.accs_train[i]) for i in idxs]) 
     results['max_test_acc'] = np.array([max(results.accs_test[i]) for i in idxs])
-    results['corr0_final'] = np.array([results.corr0[i][-1] for i in idxs])
-    results['corr1_final'] = np.array([results.corr1[i][-1] for i in idxs])
-    results['corr0_adj_final'] = np.array([results.corr0_adj[i][-1] for i in idxs])
-    results['corr1_adj_final'] = np.array([results.corr1_adj[i][-1] for i in idxs])
-    # results['corr0_adj_final'] = np.array([results.corr0_adj[i][-1] for i in range(len(results))])
-    # results['corr1_adj_final'] = np.array([results.corr1_adj[i][-1] for i in range(len(results))])
-    results['fc0_fro_final'] = np.array([results.fc0_fro[i][-1] for i in idxs]) #[results.iloc[row_num]['weight_norms'][t_max_w]['fc.0.weight_fro'] for row_num in results.index]
-    results['fc1_fro_final'] = np.array([results.fc1_fro[i][-1] for i in idxs])
-    results['fc0_stab_rank_final'] = np.array([results.fc0_stab_rank[i][-1] for i in idxs]) 
-    results['fc1_stab_rank_final'] = np.array([results.fc1_stab_rank[i][-1] for i in idxs]) 
-    results['act0_stab_rank_final'] = np.array([results.act0_stab_rank[i][-1] for i in idxs]) 
-    results['act1_stab_rank_final'] = np.array([results.act1_stab_rank[i][-1] for i in idxs])
+
     
+    def get_last(key, i):
+        try: return results[key][i][-1]
+        except: return np.nan
+    def add_final(key): return np.array([get_last(key, i) for i in idxs])    
+    for key in ['corr0', 'corr1', 'corr0_adj', 'corr1_adj', 'corr2', 'corr3',
+                'fc0_fro', 'fc1_fro', 'fc2_fro', 'fc3_fro',
+                'fc0_stab_rank', 'fc1_stab_rank', 'act0_stab_rank', 'act1_stab_rank']:
+        results[key + '_final'] = add_final(key)
     
     return its, ts, results
